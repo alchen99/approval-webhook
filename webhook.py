c849903b7bf9c7ad8f138728221c8d4a7639a83c
Alice Chen <achen@informatica.com>
2019-06-27 22:12:41 -0700

    Remove label or comment depending on if needs 2nd approval

--- webhook.py
+++ webhook.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import re
+
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config, view_defaults
@@ -21,6 +23,8 @@ class PayloadView(object):
         self.request = request
         # Payload from Github, it's a dict
         self.payload = self.request.json
+        with open('/etc/github', 'r') as file:
+            self.github = Github(file.read().replace('\n', ''))
 
     @view_config(header="X-Github-Event:push")
     def payload_push(self):
@@ -41,9 +45,30 @@ class PayloadView(object):
     @view_config(header="X-Github-Event:issue_comment")
     def payload_issue_comment(self):
         """This method is a continuation of PayloadView process, triggered if
-	header HTTP-X-Github-Event type is Issue Comment"""
-	print("Issue Comment", self.payload['action'])
-	return Response("success")
+        header HTTP-X-Github-Event type is Issue Comment"""
+
+        # define GitHub repo and PR
+        repo = self.github.get_repo(self.payload['repository']['owner']['login'] + '/' + self.payload['repository']['name'])
+        pr = repo.get_pull(self.payload['issue']['number'])
+
+        print("Issue Comment", self.payload['action'])
+
+        """Check for comment with 2nd Approval. If not found then check number of approvers."""
+        if re.search('2nd Approval', self.payload['comment']['body']) is None:
+            num_approvers = len(re.findall('title=\"Approved\">(infa-\w+)<', self.payload['comment']['body']))
+            if num_approvers == 0:
+                print("PR has no approvers")
+            elif num_approvers == 1:
+                new_comment = re.sub('This PR is .*', 'This PR needs **2nd Approval**', self.payload['comment']['body'])
+                pr.create_issue_comment(new_comment)
+                print("PR needs second approver")
+            elif num_approvers >= 2:
+                labels = [label['name'] for label in self.payload['issue']['labels']]
+                if "needs-2-approvals" in labels:
+                    pr.remove_from_labels('needs-2-approvals')
+                    print("Removed label needs-2-approvals from PR!")
+
+        return Response("success")
 
     @view_config(header="X-Github-Event:ping")
     def payload_ping(self):
